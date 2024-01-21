29f6931dafae6bb40e90f6768df78a7a632aec80
Console <50532876+Consolel@users.noreply.github.com>
2024-01-20 19:53:12 -0500

    Update CustomConfig.java

--- Plugin/slimereducer/src/main/java/com/console/files/CustomConfig.java
+++ Plugin/slimereducer/src/main/java/com/console/files/CustomConfig.java
@@ -4,46 +4,40 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import java.io.File;
 import java.io.IOException;
+/*
+ * This entire class is modified code from https://www.youtube.com/@KodySimpson, thanks!
+ * All credit goes to him.
+ */
 public class CustomConfig {
     private static File file;
     private static FileConfiguration customFile;
-    public static void setup()
-    {
+    public static void setup() {
         file = new File(Bukkit.getServer().getPluginManager().getPlugin("SlimeReducer").getDataFolder(), "config.yml");
-        if (!file.exists())
-        {
-            try
-            {
+        if (!file.exists()) {
+            try {
                 file.createNewFile();
             }
-            catch (IOException e)
-            {
+            catch (IOException e) {
                 System.out.println("[SlimeReducer] Could not create config file!");
             }
         }
         customFile = YamlConfiguration.loadConfiguration(file);
     }
  
-    public static FileConfiguration get()
-    {
+    public static FileConfiguration get() {
         return customFile;
     }
  
-    public static void save(){
-        try
-        {
+    public static void save() {
+        try {
             customFile.save(file);
         }
-        catch (IOException e)
-        {
+        catch (IOException e) {
             System.out.println("[SlimeReducer] Couldn't Save Config File!");
         }
     }
  
-    public static void reload()
-    {
+    public static void reload() {
         customFile = YamlConfiguration.loadConfiguration(file);
     }
- 
 }
-
