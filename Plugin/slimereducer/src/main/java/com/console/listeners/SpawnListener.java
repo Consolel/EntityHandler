e29e8b12ce037173457aaf0bd7ef0c4edfa8698b
Console <50532876+Consolel@users.noreply.github.com>
2024-01-20 19:52:45 -0500

    Update SpawnListener.java

--- Plugin/slimereducer/src/main/java/com/console/listeners/SpawnListener.java
+++ Plugin/slimereducer/src/main/java/com/console/listeners/SpawnListener.java
@@ -1,18 +1,14 @@
 package com.console.listeners;
-
+import com.console.files.CustomConfig;
 import org.bukkit.entity.Slime;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
-import com.console.files.CustomConfig;
 import org.bukkit.event.entity.EntitySpawnEvent;
-
 public final class SpawnListener implements Listener {
-    
     @EventHandler
     void EntitySpawnEvent(EntitySpawnEvent evt) {
         final int percentage = CustomConfig.get().getInt("Reduction");
         final boolean hasPercent = percentage > (1 + (int) (Math.random() * 100));
-        
         if(evt.getEntity() instanceof Slime && hasPercent)  {
             evt.getEntity().remove();
         }
